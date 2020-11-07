24a8974e47542fc2f81db8ffef650eff5e50ebcb
ilya07092000 <ischenko2012@gmail.com>
2020-11-07 18:48:24 +0200

    Canvas picture desktop

--- src/js/script.js
+++ src/js/script.js
@@ -109,3 +109,84 @@ let reloadBtn = document.querySelector('.reload');
 reloadBtn.addEventListener('click', () => {
     App.init();
 })
+
+// PICTURE
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.lineWidth = 4;
+
+//Нижняя дуга
+ctx.beginPath();
+ctx.bezierCurveTo(200, 400, 320, 380, 420, 400); 
+ctx.stroke()
+
+//Основа ( палка )
+ctx.beginPath();
+ctx.bezierCurveTo(310, 385, 320, 200, 310, 100); 
+ctx.stroke();
+
+// Верхняя палка
+ctx.beginPath();
+ctx.bezierCurveTo(312, 100, 250, 90, 130, 100); 
+ctx.stroke();
+
+ctx.lineWidth = 1;
+
+//Основа ( палка )
+ctx.beginPath();
+ctx.bezierCurveTo(150, 100, 148, 130, 150, 160); 
+ctx.stroke();
+
+// Голова
+ctx.beginPath();
+ctx.arc(150,185,26,0,Math.PI*2,true);
+// Левый глаз (крест)
+ctx.moveTo(138, 175);
+ctx.lineTo(145, 180);
+ctx.moveTo(145, 175);
+ctx.lineTo(138, 180);
+
+// Правый глаз (крест)
+ctx.moveTo(160, 175);
+ctx.lineTo(153, 180);
+ctx.moveTo(153, 175);
+ctx.lineTo(160, 180);
+ctx.stroke();
+
+// Рот
+ctx.moveTo(0, 0);
+ctx.beginPath();
+ctx.bezierCurveTo(136, 195, 150, 185, 165, 195); 
+ctx.stroke();
+
+// Туловище
+ctx.beginPath();
+ctx.moveTo(150, 210)
+ctx.lineTo(150, 280)
+ctx.stroke();
+
+// Левая рука
+ctx.beginPath();
+ctx.moveTo(150, 212)
+ctx.lineTo(120, 240)
+ctx.stroke();
+
+// Правая рука
+ctx.beginPath();
+ctx.moveTo(150, 212)
+ctx.lineTo(180, 240)
+ctx.stroke();
+
+// Левая нога
+ctx.beginPath();
+ctx.moveTo(150, 280)
+ctx.lineTo(130, 350)
+ctx.stroke();
+
+// Правая нога
+ctx.beginPath();
+ctx.moveTo(150, 280)
+ctx.lineTo(180, 350)
+ctx.stroke();
